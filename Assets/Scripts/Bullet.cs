5504b0b772a3d26266406afe3349304fe5320962
Jay <jsternfield@hotmail.com>
2015-01-15 23:36:59 -0500

    finished tutorial
    added Teddy, Defensepoint, Bullet, and SniperTower scripts
    placed building points and linked to spawn towers, which shoot bullets
    tower has health, and dead teddys give gold

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour
+{
+	// fly speed
+	public float speed = 10.0f;
+	
+	// destination set by Tower when creating the bullet
+	Transform destination;    
+	
+	// Update is called once per frame
+	void Update () {
+		// destroy bullet if destination does not exist anymore
+		if (destination == null)
+		{
+			Destroy(gameObject);
+			return;
+		}
+		
+		// fly towards the destination
+		float stepSize = Time.deltaTime * speed;
+		transform.position = Vector3.MoveTowards(transform.position, destination.position, stepSize);
+		
+		// reached?
+		if (transform.position.Equals(destination.position))
+		{
+			// decrease teddy health
+			Teddy t = destination.GetComponent<Teddy>();
+			t.health = t.health - 1;
+			
+			// call Teddy's onDead if died
+			if (t.health <= 0)
+			{
+				t.onDeath();
+			}
+
+			// destroy bullet
+			Destroy(gameObject);
+		}
+	}
+	
+	public void setDestination(Transform v)
+	{
+		destination = v;
+	}
+}
---
+++ Assets/Scripts/Teddy.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teddy : MonoBehaviour
+{
+	public int health = 10;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// set textmesh text
+		TextMesh tm = GetComponentInChildren<TextMesh>();
+		tm.text = new string('-', health);
+		
+		// set textmesh color to red
+		tm.renderer.material.color = Color.red;
+		
+		// adjust health bar so it always faces the camera
+		tm.transform.forward = Camera.main.transform.forward;
+	}
+	
+	public void onDeath()
+	{        
+		// increase player gold
+		Player.gold = Player.gold + 1;
+		
+		// destroy
+		Destroy(gameObject); 
+	}
+}
