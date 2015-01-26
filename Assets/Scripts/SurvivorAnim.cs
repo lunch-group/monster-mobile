9682624efc8a34d58450a66a5923f9663f00451d
Comendoza <chrisomendoza@gmail.com>
2015-01-26 09:49:03 -0500

    Edited character scene, added anim script

---
+++ Assets/Scripts/SurvivorAnim.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SurvivorAnim : MonoBehaviour 
+{
+    Animator anim;
+	int buildHash = Animator.StringToHash("bool_build");
+
+    void Start ()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update ()
+    {
+		float move = Input.GetAxis("Vertical");  // Grab outside input and pass to variable
+		anim.SetFloat("float_speed", move); //pass variable to the float parameter in the animator
+		
+		if(Input.GetKey(KeyCode.Space))
+		{
+			anim.SetBool(buildHash, true);
+			
+		}
+		
+	
+    }
+}
